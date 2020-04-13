ea51622e617b22ed1d846070cbe8a4e79b275621
YOUR USERNAMErobpwatkins <robpwatkins@gmail.com>
2020-04-13 15:13:51 -0500

    scrollToView is working on every little link. bonus.

--- app.js
+++ app.js
@@ -1,9 +1,48 @@
 const footer = document.querySelector('footer');
 const header = document.querySelector('header');
 const circleButton = document.querySelector('.circle-button');
+const homeContent = document.querySelector('.tippy-top');
+const aboutContent = document.querySelector('.about-content');
+const portfolioContent = document.querySelector('.portfolio-content');
+const contactContent = document.querySelector('.contact-content');
+const about = document.querySelector('.about');
+const portfolio = document.querySelector('.portfolio');
+const contact = document.querySelector('.contact');
+const headerHome = document.querySelector('header .home');
+const headerAbout = document.querySelector('header .about');
+const headerPortfolio = document.querySelector('header .portfolio');
+const headerContact = document.querySelector('header .contact');
+
+
+about.addEventListener('click', () => {
+  aboutContent.scrollIntoView();
+})
+
+portfolio.addEventListener('click', () => {
+  portfolioContent.scrollIntoView();
+})
+
+contact.addEventListener('click', () => {
+  contactContent.scrollIntoView();
+})
+
+headerHome.addEventListener('click', () => {
+  homeContent.scrollIntoView();
+})
+
+headerAbout.addEventListener('click', () => {
+  aboutContent.scrollIntoView();
+})
+
+headerPortfolio.addEventListener('click', () => {
+  portfolioContent.scrollIntoView();
+})
+
+headerContact.addEventListener('click', () => {
+  contactContent.scrollIntoView();
+})
 
 function obCallBack(payload) {
-  console.log(payload[0].intersectionRatio);
   if (!payload[0].isIntersecting) {
     function getHeight() {
       header.style.display = 'block';
