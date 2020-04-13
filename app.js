690810b64b39c470a6709365f981c9eaeed42918
YOUR USERNAMErobpwatkins <robpwatkins@gmail.com>
2020-04-13 09:44:07 -0500

    starting on the static

---
+++ app.js
@@ -0,0 +1,11 @@
+// function buttonClicked () {
+//   document.querySelector('.titleBox').classList.add('titleBoxTab');
+//   document.querySelector('.first').classList.add('firstTab');
+//   document.querySelector('.webDev').classList.add('webDevTab');
+//   document.querySelector('.webby').classList.add('webbyTab');
+//   document.querySelector('.tabs').classList.add('tabsTab');
+//   document.querySelector('button').classList.add('buttonTab');
+//   document.querySelector('[aria-labelledby="about"]').hidden = false;
+// }
+
+// document.querySelectorAll('button').forEach(button => button.addEventListener('click', buttonClicked));
